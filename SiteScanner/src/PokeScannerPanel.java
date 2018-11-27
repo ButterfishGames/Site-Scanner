b1727f6e4ea6170e2ff2ea000fd546cd03a40f02
Daimee MacIsaac <gingah.ninja47@gmail.com>
2018-11-27 13:19:39 -0500

    Implemented JSoup in PokeScannerPanel

--- SiteScanner/src/PokeScannerPanel.java
+++ SiteScanner/src/PokeScannerPanel.java
@@ -2,7 +2,81 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class PokeScannerPanel 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
+public class PokeScannerPanel extends JPanel
 {
+    private String url, outCode;
+    private JPanel in, out;
+    private JLabel label;
+    private JButton run;
+    private JTextField input, output;
     
+    public PokeScannerPanel()
+    {
+        setLayout(new BorderLayout());
+        
+        url = "https://bulbapedia.bulbagarden.net/wiki/";
+        outCode = "";
+        
+        label = new JLabel("Pokemon: ");
+        
+        run = new JButton("Run");
+        run.addActionListener(new RunListener());
+        
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(100, 20));
+        output = new JTextField();
+        output.setPreferredSize(new Dimension(500, 500));
+        
+        in = new JPanel();
+        in.add(label);
+        in.add(input);
+        in.add(run);
+        add(in, BorderLayout.NORTH);
+        
+        out = new JPanel();
+        out.add(output);
+        add(out, BorderLayout.SOUTH);
+    }
+    
+    private class RunListener implements ActionListener
+    {
+        public void actionPerformed (ActionEvent event)
+        {
+            String inName, outName;
+            
+            inName = input.getText();
+            outName = "";
+            
+            for (int i = 0; i < inName.length(); i++)
+            {
+                if (i == 0)
+                {
+                    outName = ("" + inName.charAt(i)).toUpperCase();
+                }
+                else
+                {
+                    outName += ("" + inName.charAt(i)).toLowerCase();
+                }
+            }
+            
+            url += outName + "_(Pokémon)";
+            
+            try
+            {
+                Document document = Jsoup.connect(url).get();
+                
+            }
+            catch (Exception e)
+            {
+                System.out.print(e);
+            }
+            output.setText(outCode);
+        }
+    }
 }
--- SiteScanner/src/SiteScanner.java
+++ SiteScanner/src/SiteScanner.java
@@ -1,41 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.awt.*;
+import javax.swing.*;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-/**
- *
- * @author dmacisaa
- */
 public class SiteScanner {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String url = "https://stackoverflow.com/questions/2835505";
-        try
-        {
-            Document document = Jsoup.connect(url).get();
-
-            String question = document.select("#question .post-text").text();
-            System.out.println("Question: " + question);
-
-            Elements answerers = document.select("#answers .user-details a");
-            for (Element answerer : answerers) 
-            {
-                System.out.println("Answerer: " + answerer.text());
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.print(e);
-        }
+        JFrame frame = new JFrame("Pokemon Scanner");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new PokeScannerPanel());
+        frame.pack();
+        frame.setVisible(true);
     }
 }
